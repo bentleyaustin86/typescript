ac6fb05d59a553d340d691378e4d31ff8737cb2c
bentleyaustin86 <bentleyaustin86@gmail.com>
2017-08-31 09:11:32 -0600

    first commit

---
+++ 001-010/006_variables.js
@@ -0,0 +1,14 @@
+// var fullName : string = "Austin Bentley";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Ian Bentley";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Austin", "Bentley");
+//# sourceMappingURL=006_variables.js.map
---
+++ 001-010/006_variables.ts
@@ -0,0 +1,17 @@
+// var fullName : string = "Austin Bentley";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Ian Bentley";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+  let greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName("Austin", "Bentley");
